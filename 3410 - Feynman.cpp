42f751f9999f7c470dc9863732d8574308f5c561
T-Jain <jain.tushar@outlook.com>
2015-09-23 01:17:21 +0530

    Solution to Problem #3410

---
+++ 3410 - Feynman.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+using namespace std;
+
+int main() {
+	int n;
+	while (1) {
+		scanf("%d",&n);
+		if (n==0)
+			break;
+		else
+			printf("%d\n",(n*(n+1)*(2*n+1))/6);
+	}
+	return 0;
+}
